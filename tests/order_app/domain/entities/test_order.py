2686b6c3f5316e576e53d79bf5a82ef4a72ffee4
Hassan Mohagheghian <hassan.mohagheghian.cs@gmail.com>
2025-12-15 18:26:57 +0330

    add: tests for edit/delete item in order

--- tests/order_app/domain/entities/test_order.py
+++ tests/order_app/domain/entities/test_order.py
@@ -33,3 +33,40 @@ def test_add_order_item(order):
     assert order.items[0] == OrderItem(
         product_id=product.id, quantity=2, price_per_unit=product.price
     )
+
+
+def test_delete_order_item(order):
+    product = Product(
+        name="Test Product",
+        description="A test product",
+        price=Money(Decimal("15.00")),
+        stock_quantity=10,
+    )
+    order.add_item(product=product, quantity=2)
+    assert order.item_count == 1
+    order.remove_item(order.items[0])
+    assert order.item_count == 0
+
+
+def test_edit_order_item(order):
+    product1 = Product(
+        name="Test Product1",
+        description="A test product1",
+        price=Money(Decimal("15.00")),
+        stock_quantity=10,
+    )
+
+    product2 = Product(
+        name="Test Product",
+        description="A test product",
+        price=Money(Decimal("20.00")),
+        stock_quantity=25,
+    )
+    order.add_item(product=product1, quantity=2)
+    order.add_item(product=product2, quantity=5)
+    assert order.item_count == 2
+
+    order.edit_item(product1.id, 3)
+
+    assert order.items[0].quantity == 3
+    assert order.items[1].quantity == 5
